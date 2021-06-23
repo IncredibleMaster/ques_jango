f66fe6168c4560cb70bddb19f847ee6bf8d4f0fd
IncredibleMaster <golden.forest129@gmail.com>
2021-06-23 18:00:39 -0400

    First commit

---
+++ core/views.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.base import TemplateView
+
+
+class IndexTemplateView(LoginRequiredMixin, TemplateView):
+
+    def get_template_names(self):
+        if not settings.DEBUG:
+            template_name = "index-dev.html"
+        else:
+            template_name = "index.html"
+        return template_name
